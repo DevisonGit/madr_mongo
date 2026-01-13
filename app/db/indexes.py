dcfc7bc9a91718b053c394ccd430950ab1e9ee7d
devisono <devison_santos@yahoo.com.br>
2026-01-13 00:56:51 -0300

    fix: indexes

--- app/db/indexes.py
+++ app/db/indexes.py
@@ -1,30 +1,30 @@
 from pymongo import ASCENDING
 
-
 INDEX_DEFINITIONS = {
-    "users": [
+    'users': [
         {
-            "keys": [("email", ASCENDING)],
-            "options": {"unique": True},
+            'keys': [('email', ASCENDING)],
+            'options': {'unique': True},
         },
         {
-            "keys": [("username", ASCENDING)],
-            "options": {"unique": True},
-        },
-    ],
-    "authors": [
-        {
-            "keys": [("name", ASCENDING)],
-            "options": {"unique": True},
+            'keys': [('username', ASCENDING)],
+            'options': {'unique': True},
         },
     ],
-    "books": [
+    'authors': [
         {
-            "keys": [("title", ASCENDING)],
-            "options": {"unique": True},
+            'keys': [('name', ASCENDING)],
+            'options': {'unique': True},
+        },
+    ],
+    'books': [
+        {
+            'keys': [('title', ASCENDING)],
+            'options': {'unique': True},
         },
     ],
 }
+
 
 async def create_indexes(db):
     for collection_name, indexes in INDEX_DEFINITIONS.items():
@@ -33,12 +33,11 @@ async def create_indexes(db):
         existing_indexes = await collection.index_information()
 
         for index in indexes:
-            keys = index["keys"]
-            options = index.get("options", {})
+            keys = index['keys']
+            options = index.get('options', {})
 
             # nome padrão do mongo (email_1, name_lower_1, etc)
-            index_name = "_".join(f"{k}_{v}" for k, v in keys)
+            index_name = '_'.join(f'{k}_{v}' for k, v in keys)
 
             if index_name not in existing_indexes:
                 await collection.create_index(keys, **options)
-
